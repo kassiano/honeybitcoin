67d22f910ffded1924b3e2d98ec80e2456ec3c0f
kassiano <kassiano.resende@gmail.com>
2018-03-07 09:26:27 -0300

    google verification

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="google-site-verification" content="28J19BM2v_REB6OPZ5Mft5kkrcGa5AAFF13Nrm5WekE" />
     <title>Honey bitcoin</title>
   </head>
   <body>
